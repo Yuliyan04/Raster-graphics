d3ebafa9e5808ff4ca19851fd0d7fe7d43e2ffd7
Yuliyan04 <147076143+Yuliyan04@users.noreply.github.com>
2024-06-17 16:49:11 +0300

    Removing rotation functions

--- Raster graphics/ImagePBM.h
+++ Raster graphics/ImagePBM.h
@@ -11,12 +11,13 @@ public:
     ImagePBM(unsigned width, unsigned height, const String& magicNumber, const String& fileName, const BitSet& data);
     ImagePBM(unsigned width, unsigned height, const String& magicNumber, const String& fileName, BitSet&& data);
 
+
     void grayscale() override;
     void monochrome() override;
     void negative() override;
-    void rotate(const String& direction) override;
-
+    
     void save() const override;
+
 
     virtual Image* clone() const override;
 };
