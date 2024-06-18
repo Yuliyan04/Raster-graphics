ceb890e25d5ebdac0c314b0c8716a3c44c4c0901
Yuliyan04 <147076143+Yuliyan04@users.noreply.github.com>
2024-06-18 21:15:52 +0300

    commit

--- Raster graphics/Session.h
+++ Raster graphics/Session.h
@@ -1,45 +1,14 @@
 #pragma once
 #include "ImageCollection.h"
+#include "ImageFactory.h"
+#include "ImageUtilities.h"
 #include "String.h"
 #include "Vector.hpp"
+#include <sstream>
+
 
 
 class Session
 {
-private:
-    int sessionId;
-    ImageCollection images;
-    Vector<String> unsavedChanges;
-
-public:
-    static int idCounter;
-
-    Session();
-    Session(const Session& other);
-    Session(Session&& other) noexcept;
-    Session& operator=(const Session& other);
-    Session& operator=(Session&& other) noexcept;
-    ~Session();
-
-    void grayscale();
-    void monochrome();
-    void negative();
-
-    void addImage(Image* image);
-    void rotate(const String& direction);
-    void undo();
-    void close();
-    String info() const;
-
-    bool containsFile(const String& fileLocation) const;
-    bool hasUnsavedChanges() const;
-
-    int getId() const;
-
-private:
-    void saveImageToFile(Image* image, const String& fileName);
-    void free();
-    void copyFrom(const Session& other);
-    void moveFrom(Session&& other);
 };
 
