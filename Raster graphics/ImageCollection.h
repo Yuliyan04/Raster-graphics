45b4e8ae6847469a2a5f0e930ec7368a30b6d2da
Yuliyan04 <147076143+Yuliyan04@users.noreply.github.com>
2024-06-18 13:17:07 +0300

    ImageCollection

---
+++ Raster graphics/ImageCollection.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Image.h"
+	
+class ImageCollection
+{
+private:
+    Image** data;
+    size_t capacity;
+    size_t size;
+
+public:
+    ImageCollection();
+    ImageCollection(const ImageCollection& other);
+    ImageCollection(ImageCollection&& other) noexcept;
+    ImageCollection& operator=(const ImageCollection& other);
+    ImageCollection& operator=(ImageCollection&& other) noexcept;
+    ~ImageCollection();
+
+    void addImage(Image* newImage);
+
+    size_t getSize() const;
+    Image* getImage(size_t index) const;
+    Image* removeImage(size_t index);
+
+private:
+    void resize(size_t newCap);
+    void free();
+    void copyFrom(const ImageCollection& other);
+    void moveFrom(ImageCollection&& other);
+};
+
